10fd3122336fdf3f3ffd730c3d0f4239a5810e99
Armin <asteudte@gmx.de>
2017-04-15 18:19:16 +0200

    Berechnung der Kurstage implementiert

--- garde-sheet-generator/src/main/java/de/as/garde_sheet_generator/service/GradeSheetService.java
+++ garde-sheet-generator/src/main/java/de/as/garde_sheet_generator/service/GradeSheetService.java
@@ -1,12 +1,58 @@
 package de.as.garde_sheet_generator.service;
 
+import java.security.spec.EncodedKeySpec;
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.google.common.annotations.VisibleForTesting;
+
 import de.as.garde_sheet_generator.dao.HolidayDao;
 import de.as.garde_sheet_generator.dao.PupilDao;
 
 public class GradeSheetService {
-	
+
+	private final HolidayDao holidays;
+	private final PupilDao pupils;
+
 	public GradeSheetService(HolidayDao holidaySupplier, PupilDao pupilSupplier) {
-		// TODO Auto-generated constructor stub
+		this.pupils = pupilSupplier;
+		this.holidays = holidaySupplier;
+	}
+
+	@VisibleForTesting
+	protected List<LocalDate> calculateLessonDates(Set<DayOfWeek> lessonWeekdays, LocalDate schoolYearStart,
+			LocalDate schoolYearEnd) {
+
+		List<LocalDate> lessonDates = new ArrayList<>();
+
+		final LocalTime placeholderTime = LocalTime.of(0, 0);
+		final LocalDateTime startDateTime = LocalDateTime.of(schoolYearStart, placeholderTime);
+		final LocalDateTime endDate = LocalDateTime.of(schoolYearEnd, placeholderTime);
+
+		long differenceInDays = Duration.between(startDateTime, endDate).toDays();
+
+		LocalDate currentDate = schoolYearStart;
+		List<LocalDate> allDates = new ArrayList<>();
+
+		for (int i = 0; i < differenceInDays; i++) {
+
+			allDates.add(currentDate);
+
+			currentDate = currentDate.plusDays(1);
+		}
+
+		lessonDates = allDates.stream().filter(d -> lessonWeekdays.contains(d.getDayOfWeek()))
+				.collect(Collectors.toList());
+
+		return lessonDates;
+
 	}
 
 }
--- garde-sheet-generator/src/test/java/de/as/garde_sheet_generator/service/GradeSheetServiceTest.java
+++ garde-sheet-generator/src/test/java/de/as/garde_sheet_generator/service/GradeSheetServiceTest.java
@@ -1,25 +1,55 @@
 package de.as.garde_sheet_generator.service;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
 import de.as.garde_sheet_generator.dao.HolidayDao;
+import de.as.garde_sheet_generator.dao.PupilDao;
 
 public class GradeSheetServiceTest {
 
-	private HolidayDao dao;
+	private HolidayDao holidaySupplier;
+	private PupilDao pupilSupplier;
+	
+	private GradeSheetService service;
 	
 	@Before
 	public void setUp() {
-		this.dao = mock(HolidayDao.class);
+		this.holidaySupplier = mock(HolidayDao.class);
+		this.pupilSupplier = mock(PupilDao.class);
+		
+		this.service = new GradeSheetService(holidaySupplier, pupilSupplier);
 	}
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void calculateLessonDates_WithOneWeek_ShouldReturnSpecifiedWeekdaysAsConcreteDates() {
+
+		// given
+		Set<DayOfWeek> lessonDays = Sets.newHashSet(DayOfWeek.MONDAY, DayOfWeek.WEDNESDAY);
+		LocalDate schoolYearStart = LocalDate.of(2017, 4, 10); // Monday
+		LocalDate schoolYearEnd	= LocalDate.of(2017, 4, 16); // Sunday
+		
+		// when
+		List<LocalDate> actualLessonDates = this.service.calculateLessonDates(lessonDays, schoolYearStart, schoolYearEnd);
+		
+		// then
+		assertThat(actualLessonDates.size(), is(2));
+		List<DayOfWeek> actualDaysOfWeek = actualLessonDates.stream().map((date) -> DayOfWeek.from(date)).collect(Collectors.toList());
+		assertThat(actualDaysOfWeek.get(0), is(DayOfWeek.MONDAY));
+		assertThat(actualDaysOfWeek.get(1), is(DayOfWeek.WEDNESDAY));
 	}
 
 }
